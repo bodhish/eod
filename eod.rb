72e226c854fa438627e5847f452390062a91381d
Bodhish Thomas <bodhish@gmail.com>
2024-05-01 20:28:00 +0530

    Rewamp the script to output friendly messages

--- eod.rb
+++ eod.rb
@@ -4,18 +4,21 @@ require 'time'
 
 class GitHubActivityLogger
   GITHUB_API_BASE = "https://api.github.com".freeze
-  NUMBER_OF_HOURS = 24
+  NUMBER_OF_HOURS = 50
   TIME_DIFFERENCE = NUMBER_OF_HOURS * 3600
 
   def initialize(username = ENV['GITHUB_USERNAME'], token = ENV['GITHUB_TOKEN'])
     @username = username
     @token = token
+    @events_cache = []
   end
 
   def log_activities
-    events = fetch_github_events
-    activity_log = process_events(events)
-    save_activity_log(activity_log)
+    puts "Starting to log activities for #{@username}..."
+    fetch_github_events
+    process_events
+    save_activity_log
+    puts "Finished logging activities for #{@username}. Check eod.md for details."
   end
 
   private
@@ -24,12 +27,16 @@ class GitHubActivityLogger
     puts "Fetching GitHub events for #{@username}..."
     uri = URI("#{GITHUB_API_BASE}/users/#{@username}/events")
     response = make_http_request(uri)
+    @events_cache = parse_response(response)
+  end
+
+  def parse_response(response)
     JSON.parse(response.body)
   rescue JSON::ParserError => e
-    puts "Error parsing the response: #{e.message}"
+    puts "Oops! There was an error parsing the response: #{e.message}"
     []
   rescue StandardError => e
-    puts "An error occurred: #{e.message}"
+    puts "Oops! Something went wrong: #{e.message}"
     []
   end
 
@@ -43,15 +50,14 @@ class GitHubActivityLogger
     http.request(request)
   end
 
-  def process_events(events)
-    activity_log = {}
-    events.each do |event|
-      process_event(event, activity_log)
+  def process_events
+    @activity_log = {}
+    @events_cache.each do |event|
+      process_event(event)
     end
-    activity_log
   end
 
-  def process_event(event, activity_log)
+  def process_event(event)
     return unless Time.parse(event['created_at']) > Time.now - TIME_DIFFERENCE
 
     event_type = event['type']
@@ -62,39 +68,39 @@ class GitHubActivityLogger
       'PushEvent' => method(:process_push_event)
     }[event_type]
 
-    event_processor.call(event, activity_log) if event_processor
+    event_processor.call(event) if event_processor
   end
 
-  def process_pr_review_event(event, activity_log)
+  def process_pr_review_event(event)
     pr_name = event['payload']['pull_request']['title']
     pr_link = event['payload']['pull_request']['html_url']
     author_name = event['payload']['pull_request']['user']['login']
-    activity_log["Reviewed PR: [#{pr_name}](#{pr_link}) by _#{author_name}_"] = nil
+    @activity_log["Reviewed a pull request: [#{pr_name}](#{pr_link}) by _#{author_name}_"] = nil
   end
 
-  def process_issue_pr_event(event, activity_log)
-    event_key = event['type'] == 'IssuesEvent' ? 'issue' : 'pull_request'
+  def process_issue_pr_event(event)
+    event_key, head_text = event['type'] == 'IssuesEvent' ? ['issue', 'Created an issue'] : ['pull_request', 'Opened a pull request']
 
     if event['payload'][event_key] && event['payload']['action'] == 'opened'
       title = event['payload'][event_key]['title']
       link = event['payload'][event_key]['html_url']
-      activity_log["Opened #{event['type'].chomp('Event')}: [#{title}](#{link})"] = nil
+      @activity_log["#{head_text}: [#{title}](#{link})"] = nil
     end
   end
 
-  def process_push_event(event, activity_log)
+  def process_push_event(event)
     repo_name = event['repo']['name']
     branch_name = event['payload']['ref'].split('/').last
-    activity_log[repo_name] ||= {}
-    activity_log[repo_name][branch_name] ||= []
+    @activity_log[repo_name] ||= {}
+    @activity_log[repo_name][branch_name] ||= []
     event['payload']['commits'].each do |commit|
-      activity_log[repo_name][branch_name] << commit['message']
+      @activity_log[repo_name][branch_name] << commit['message']
     end
   end
 
-  def save_activity_log(activity_log)
+  def save_activity_log
     log_filename = "eod.md"
-    contents = activity_log.map do |activity, details|
+    contents = @activity_log.map do |activity, details|
       if details.nil?
         "- #{activity}\n"
       else
@@ -104,6 +110,10 @@ class GitHubActivityLogger
       end
     end.join
 
+    contents = ["**#{Date.today.to_s}**","**Done**", contents].join("\n")
+    puts "----------------------------------------"
+    puts contents
+    puts "----------------------------------------"
     File.write(log_filename, contents)
     puts "Activities logged to #{log_filename}."
   end
@@ -118,6 +128,5 @@ unless github_username && github_token
   exit 1
 end
 
-puts "Logging GitHub activities for #{github_username}..."
 logger = GitHubActivityLogger.new(github_username, github_token)
 logger.log_activities
